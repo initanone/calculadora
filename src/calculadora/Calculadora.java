8c42feff616a458d8b7b9bd7a589f2edef9549fa
initanone <167250982+initanone@users.noreply.github.com>
2025-06-21 20:17:59 -0300

    upd
    att

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,9 +1,22 @@
 package calculadora;
-
+import java.util.Scanner;
 public class Calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        double numero1, numero2, resultado;
+        char operador;
+
+        System.out.println(" Calculadora ");
+
+        System.out.print("Digite o primeiro número: ");
+        numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o operador (+, -, *, /): ");
+        operador = scanner.next().charAt(0);
+
+        System.out.print("Digite o segundo número: ");
+        numero2 = scanner.nextDouble();
 
 	}
 
