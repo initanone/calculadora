8f20e1e4190bb7c57307a5d38a922bb2df4d98b1
initanone <167250982+initanone@users.noreply.github.com>
2025-06-22 10:21:24 -0300

    upd_2_calculadora
    calculadora

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -6,7 +6,7 @@ public class Calculadora {
 		Scanner scanner = new Scanner(System.in);
         double numero1, numero2, resultado;
         char operador;
-
+         
         System.out.println(" Calculadora ");
 
         System.out.print("Digite o primeiro número: ");
@@ -17,6 +17,32 @@ public class Calculadora {
 
         System.out.print("Digite o segundo número: ");
         numero2 = scanner.nextDouble();
+        switch (operador) {
+        case '+':
+            resultado = numero1 + numero2;
+            break;
+        case '-':
+            resultado = numero1 - numero2;
+            break;
+        case '*':
+            resultado = numero1 * numero2;
+            break;
+        case '/':
+            if (numero2 != 0) {
+                resultado = numero1 / numero2;
+            } else {
+                System.out.println("Erro: Divisão por zero não é permitida.");
+                return;
+            }
+            break;
+        default:
+            System.out.println("Operador inválido!");
+            return;
+    }
+
+    System.out.println("O resultado é: " + resultado);
+
+    scanner.close();
 
 	}
 
